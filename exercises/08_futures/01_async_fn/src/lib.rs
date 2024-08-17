9366ebe3da8bf7c103a93f973d116d1f3070586e
Muhammad Rizki Darmawan <muhrizdar@gmail.com>
2024-08-18 05:09:27 +0700

    all done

--- exercises/08_futures/01_async_fn/src/lib.rs
+++ exercises/08_futures/01_async_fn/src/lib.rs
@@ -1,3 +1,5 @@
+use std::borrow::BorrowMut;
+
 use tokio::net::TcpListener;
 
 // TODO: write an echo server that accepts incoming TCP connections and
@@ -11,7 +13,13 @@ use tokio::net::TcpListener;
 // - `tokio::net::TcpStream::split` to obtain a reader and a writer from the socket
 // - `tokio::io::copy` to copy data from the reader to the writer
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    loop {
+        let (mut conn, _) = listener.accept().await.unwrap();
+        let (mut reader, mut writer) = conn.split();
+        tokio::io::copy(reader.borrow_mut(), writer.borrow_mut())
+            .await
+            .unwrap();
+    }
 }
 
 #[cfg(test)]
